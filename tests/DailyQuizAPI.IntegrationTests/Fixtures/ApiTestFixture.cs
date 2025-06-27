f15361c214aaeb6d72d7954643f1a0be68a5dd2d
Lambert Maxime <lambert.maxime@protonmail.com>
2025-06-28 01:01:27 +0200

    feat(Sumots) : Ajout des tests d'intégration pour les endpoints créés pour le moment

---
+++ tests/DailyQuizAPI.IntegrationTests/Fixtures/ApiTestFixture.cs
@@ -0,0 +1,43 @@
+﻿using DailyQuizAPI.IntegrationTests.Containers;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Testcontainers.PostgreSql;
+using Xunit;
+
+namespace DailyQuizAPI.IntegrationTests.Fixtures;
+
+public class ApiTestFixture : IAsyncLifetime
+{
+    public HttpClient Client { get; private set; } = default!;
+    public PostgreSqlContainer DbContainer { get; private set; } = default!;
+    private WebApplicationFactory<Program> _factory = default!;
+
+    public async Task InitializeAsync()
+    {
+        DbContainer = PostgresTestContainer.Create();
+        await DbContainer.StartAsync();
+
+        _factory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureAppConfiguration((_, config) =>
+                {
+                    var settings = new Dictionary<string, string?>
+                    {
+                        ["ConnectionStrings:Database"] = DbContainer.GetConnectionString()
+                    };
+                    config.AddInMemoryCollection(settings!);
+                });
+            });
+
+        Client = _factory.CreateClient();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await DbContainer.StopAsync();
+        Client.Dispose();
+        _factory.Dispose();
+    }
+}
+
---
+++ tests/DailyQuizAPI.IntegrationTests/Sumots/GetSumotsTests.cs
@@ -0,0 +1,26 @@
+﻿using DailyQuizAPI.IntegrationTests.Fixtures;
+using DailyQuizAPI.Sumots.GetSumots;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace DailyQuizAPI.IntegrationTests.Sumots;
+
+public class GetSumotsTests(ApiTestFixture fixture) : IClassFixture<ApiTestFixture>
+{
+    private readonly HttpClient _client = fixture.Client;
+
+    [Fact]
+    public async Task GetSumots_ReturnsOk_WithExpectedShape()
+    {
+        var response = await _client.GetAsync("/FiveLettersFrenchWords");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        response = await _client.GetAsync("/sumots");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var content = await response.Content.ReadFromJsonAsync<List<GetSumotsResponse>>();
+        content.Should().NotBeNull().And.HaveCountGreaterThan(0);
+    }
+}
