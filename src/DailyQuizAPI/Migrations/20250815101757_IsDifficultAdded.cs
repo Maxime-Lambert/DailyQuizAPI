86f65b7f14674942075cd09370487cda96b91a0d
Lambert Maxime <lambert.maxime@protonmail.com>
2025-08-16 08:22:43 +0200

    feat : L'extraction de Sumots utilise désormais Open Lexical
    Correction de UpdateSumots / Ajout de IsDifficult dans les Sumots

---
+++ src/DailyQuizAPI/Migrations/20250815101757_IsDifficultAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DailyQuizAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsDifficultAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDifficult",
+                table: "Sumots",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDifficult",
+                table: "Sumots");
+        }
+    }
+}
