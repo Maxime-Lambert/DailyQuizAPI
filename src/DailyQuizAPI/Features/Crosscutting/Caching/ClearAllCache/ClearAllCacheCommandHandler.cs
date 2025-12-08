bdba0e90c8834b1cf40509c99bccf2faec926f1f
Lambert Maxime <lambert.maxime@protonmail.com>
2025-12-08 16:02:09 +0100

    feat (CrossCutting) : Ajout d'un endpoint pour supprimer tout le cache

---
+++ src/DailyQuizAPI/Features/Crosscutting/Caching/ClearAllCache/ClearAllCacheCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace DailyQuizAPI.Features.Crosscutting.Caching.ClearAllCache;
+
+public class ClearAllCacheCommandHandler(ICacheService cacheService)
+{
+    private readonly ICacheService _cacheService = cacheService;
+
+    public Task Handle()
+    {
+        _cacheService.RemoveByPrefix("");
+        return Task.CompletedTask;
+    }
+}
